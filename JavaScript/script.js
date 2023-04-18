8fbf0424b6de73b74adac20b5493e802727e26f0
christian6404 <christian6404@gmail.com>
2023-04-18 10:31:41 -0400

    fixed file structure

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -29,3 +29,150 @@ calculateBtn.addEventListener(`click`, () => {
 
 
 // BMR Calculator START
+
+
+var workoutlist = {
+    arm: function () {
+        let options = {
+            method: 'GET',
+            headers: { 'x-api-key': 'MEse9WuVgjjzP6YYlCv3pg==s93de26XwygSE5iO' }
+          }
+          
+          let url = 'https://api.api-ninjas.com/v1/exercises?muscle=biceps'
+
+          fetch(url,options)
+          .then(function (response) {
+              return response.json()
+          })
+          .then(function (data) {
+              console.log(data)
+          })
+    },
+
+    abs: function () {
+        let options = {
+            method: 'GET',
+            headers: { 'x-api-key': 'MEse9WuVgjjzP6YYlCv3pg==s93de26XwygSE5iO' }
+          }
+          
+          let url = 'https://api.api-ninjas.com/v1/exercises?muscle=abdominals'
+
+          fetch(url,options)
+          .then(function (response) {
+            return response.json()
+        }) 
+          .then(function(data) {
+            console.log(data)
+          })
+    },
+
+    back: function () {
+        let options = {
+            method: 'GET',
+            headers: { 'x-api-key': 'MEse9WuVgjjzP6YYlCv3pg==s93de26XwygSE5iO' }
+          }
+          
+          let url = 'https://api.api-ninjas.com/v1/exercises?muscle=lower_back'
+
+          fetch(url,options)
+          .then(function (response) {
+            return response.json()
+        }) 
+          .then(function(data) {
+            console.log(data)
+          })
+    },
+
+    calves: function () {
+        let options = {
+            method: 'GET',
+            headers: { 'x-api-key': 'MEse9WuVgjjzP6YYlCv3pg==s93de26XwygSE5iO' }
+          }
+          
+          let url = 'https://api.api-ninjas.com/v1/exercises?muscle=calves'
+
+          fetch(url,options)
+          .then(function (response) {
+            return response.json()
+        }) 
+          .then(function(data) {
+            console.log(data)
+          })
+    },
+
+    cardio: function () {
+        let options = {
+            method: 'GET',
+            headers: { 'x-api-key': 'MEse9WuVgjjzP6YYlCv3pg==s93de26XwygSE5iO' }
+          }
+          
+          let url = 'https://api.api-ninjas.com/v1/exercises?type=cardio'
+
+          fetch(url,options)
+          .then(function (response) {
+            return response.json()
+        }) 
+          .then(function(data) {
+            console.log(data)
+          })
+    },
+
+    chest: function () {
+        let options = {
+            method: 'GET',
+            headers: { 'x-api-key': 'MEse9WuVgjjzP6YYlCv3pg==s93de26XwygSE5iO' }
+          }
+          
+          let url = 'https://api.api-ninjas.com/v1/exercises?muscle=chest'
+
+          fetch(url,options)
+          .then(function (response) {
+            return response.json()
+        }) 
+          .then(function(data) {
+            console.log(data)
+          })
+    },
+
+    legs: function () {
+        let options = {
+            method: 'GET',
+            headers: { 'x-api-key': 'MEse9WuVgjjzP6YYlCv3pg==s93de26XwygSE5iO' }
+          }
+          
+          let url = 'https://api.api-ninjas.com/v1/exercises?muscle=hamstrings'
+
+          fetch(url,options)
+          .then(function (response) {
+            return response.json()
+        }) 
+          .then(function(data) {
+            console.log(data)
+          })
+    },
+
+    shoulder: function () {
+        let options = {
+            method: 'GET',
+            headers: { 'x-api-key': 'MEse9WuVgjjzP6YYlCv3pg==s93de26XwygSE5iO' }
+          }
+          
+          let url = 'https://api.api-ninjas.com/v1/exercises?muscle=traps'
+
+          fetch(url,options)
+          .then(function (response) {
+            return response.json()
+        }) 
+          .then(function(data) {
+            console.log(data)
+          })
+    }
+}
+
+workoutlist.arm()
+workoutlist.abs()
+workoutlist.back()
+workoutlist.calves()
+workoutlist.chest()
+workoutlist.legs()
+workoutlist.shoulder()
